4d296a1088e011daa46c5b08950edf496442e264
Emil Reyier Hedin <92026465+jediemil@users.noreply.github.com>
2023-04-05 23:53:17 +0200

    ADD WIFI PASS FOR THE THIRD TIME BECAUSE GIT DOES NOT WANT DO COOPERATE (probably my fault)

---
+++ src/WiFiPass.h
@@ -0,0 +1,11 @@
+//
+// Created by emilr on 2023-03-24.
+//
+
+#ifndef NIXIECLOCK_WIFIPASS_H
+#define NIXIECLOCK_WIFIPASS_H
+
+const char* ssid = "WIFI SSID";
+const char* password = "WIFI PASSWORD";
+
+#endif //NIXIECLOCK_WIFIPASS_H
